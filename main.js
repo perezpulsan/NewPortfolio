7b09e6cec427125f0b1071199f63c3c483afd5c6
perezpulsan <perezpulsan@gmail.com>
2019-09-16 21:15:45 +0200

    my first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function (){
+    $('.menu-toggler').on('click', function () {
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+    $('.top-nav .nav-link').on('click', function () {
+        $(".menu-toggler").removeClass('open');
+        $('.topnav').removeClass('open');
+    });
+
+    $('.nav a[href*="#"]').on('click', function () {
+        $("html, body").animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100 }, 2000);
+        });
+        $('.topnav').removeClass('open');
+    });
+
+    $('#up').on('click', function () {
+        $("html, body").animate({scrollTop: 0}, 2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800
+    });
